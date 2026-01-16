3fefb3fdd2309ec8943453ee309606a2d9f320a8
Peter Koban <peter.koban@pm.me>
2026-01-16 15:12:49 +0100

    feat(footer): base footer component

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar,Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
