0edb2da745090a15cefa05024cc234340a5aa40c
Peter Koban <peter.koban@pm.me>
2026-01-16 20:37:11 +0100

    fix(settings): Cache options

--- frontend/src/app/recipes/recipes.ts
+++ frontend/src/app/recipes/recipes.ts
@@ -1,10 +1,7 @@
-import { Component, inject, signal, OnInit, PLATFORM_ID, makeStateKey, TransferState } from '@angular/core';
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeCard, Recipe } from '../recipe-card/recipe-card';
 import { environment } from '../../environments/environment';
-
-const RECIPES_KEY = makeStateKey<Recipe[]>('recipes');
 
 @Component({
   selector: 'app-recipes',
@@ -14,23 +11,13 @@ const RECIPES_KEY = makeStateKey<Recipe[]>('recipes');
 })
 export class Recipes implements OnInit {
   private http = inject(HttpClient);
-  private transferState = inject(TransferState);
-  private platformId = inject(PLATFORM_ID);
 
   recipes = signal<Recipe[]>([]);
   loading = signal(true);
   error = signal<string | null>(null);
 
   ngOnInit(): void {
-    // Check if data was transferred from server
-    if (isPlatformBrowser(this.platformId) && this.transferState.hasKey(RECIPES_KEY)) {
-      const recipes = this.transferState.get(RECIPES_KEY, []);
-      this.recipes.set(recipes);
-      this.loading.set(false);
-      this.transferState.remove(RECIPES_KEY);
-    } else {
-      this.loadRecipes();
-    }
+    this.loadRecipes();
   }
 
   private loadRecipes(): void {
@@ -38,11 +25,6 @@ export class Recipes implements OnInit {
       next: (data) => {
         this.recipes.set(data);
         this.loading.set(false);
-
-        // Store data for transfer to browser
-        if (isPlatformServer(this.platformId)) {
-          this.transferState.set(RECIPES_KEY, data);
-        }
       },
       error: () => {
         this.error.set('Rezepte konnten nicht geladen werden.');
