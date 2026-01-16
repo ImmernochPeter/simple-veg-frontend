5786dea1c117d0a2f8c1f63060acfd9df1bd7dfd
Peter Koban <peter.koban@pm.me>
2026-01-16 20:27:59 +0100

    fix(recipes): render on server

--- frontend/src/app/recipes/recipes.ts
+++ frontend/src/app/recipes/recipes.ts
@@ -1,5 +1,4 @@
-import { Component, inject, signal, OnInit, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeCard, Recipe } from '../recipe-card/recipe-card';
 import { environment } from '../../environments/environment';
@@ -12,16 +11,13 @@ import { environment } from '../../environments/environment';
 })
 export class Recipes implements OnInit {
   private http = inject(HttpClient);
-  private platformId = inject(PLATFORM_ID);
 
   recipes = signal<Recipe[]>([]);
   loading = signal(true);
   error = signal<string | null>(null);
 
   ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.loadRecipes();
-    }
+    this.loadRecipes();
   }
 
   private loadRecipes(): void {
